b3b3a590345936a53bafdf8b0b20bfc81e64f5fb
Everett Grassler <dragwar9@gmail.com>
2019-04-03 22:14:03 -0500

    Added nav link active switching depending on current page

--- BugTracker/Scripts/MyScripts/MyScript.js
+++ BugTracker/Scripts/MyScripts/MyScript.js
@@ -1,1 +1,19 @@
-﻿
+﻿/** @description this will switch the active class on the my-page-link (nav-links) according to the current page*/
+(() => {
+    const currentPage = document.getElementById("currentPage");
+    const pageLinks = [...document.getElementsByClassName("my-page-link")];
+    let isHomePage = true;
+
+    pageLinks.forEach(element => {
+        if (currentPage.textContent == element.id) {
+            isHomePage = false;
+            element.classList.add("active");
+        } else {
+            element.classList.remove("active");
+        }
+    });
+
+    if (isHomePage) {
+        pageLinks.find(ele => ele.id == "home-index").classList.add("active");
+    }
+})();
