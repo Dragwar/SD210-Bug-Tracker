7fc990a07908cdd33781450e82e7769d5551c89f
Everett Grassler <dragwar9@gmail.com>
2019-04-14 20:54:04 -0500

    Added basic ticket details
    - Need to change in future when actual ticket features are implemented

--- BugTracker/Controllers/TicketController.cs
+++ BugTracker/Controllers/TicketController.cs
@@ -1,5 +1,6 @@
 ﻿using BugTracker.Models;
 using BugTracker.Models.ViewModels.Ticket;
+using BugTracker.MyHelpers;
 using BugTracker.MyHelpers.DB_Repositories;
 using BugTracker.MyHelpers.DB_Repositories.Ticket;
 using Microsoft.AspNet.Identity;
@@ -12,25 +13,59 @@ namespace BugTracker.Controllers
 {
     public class TicketController : Controller
     {
-        public readonly ApplicationDbContext DbContext;
-        public readonly TicketRepository TicketRepository;
-        public readonly UserRoleRepository UserRoleRepository;
+        private readonly ApplicationDbContext DbContext;
+        private readonly TicketRepository TicketRepository;
+        private readonly UserRoleRepository UserRoleRepository;
+        private readonly UserRepository UserRepository;
 
         public TicketController()
         {
             DbContext = new ApplicationDbContext();
             TicketRepository = new TicketRepository(DbContext);
+            UserRepository = new UserRepository(DbContext);
             UserRoleRepository = new UserRoleRepository(DbContext);
         }
 
         // GET: Ticket
+        [Authorize]
         public ActionResult Index()
         {
             string userId = User.Identity.GetUserId();
-            var project = DbContext.Projects.First();
-            List<TicketIndexViewModel> model = DbContext.Tickets.ToList()
-                .Select(ticket => TicketIndexViewModel.CreateViewModel(ticket))
-                .ToList();
+            ApplicationUser currentUser;
+
+            currentUser = UserRepository.GetUserById(userId);
+            if (currentUser == null)
+            {
+                return RedirectToAction(nameof(HomeController.Index), new { controller = "Home" });
+            }
+
+            List<TicketIndexViewModel> model;
+            if (User.IsInRole(nameof(UserRolesEnum.Admin)) || User.IsInRole(nameof(UserRolesEnum.ProjectManager)))
+            {
+                //! ONLY admins/project-managers can see all tickets
+                model = TicketRepository.GetAllTickets()
+                    .Select(ticket => TicketIndexViewModel.CreateViewModel(ticket))
+                    .ToList();
+            }
+            else if (User.IsInRole(nameof(UserRolesEnum.Submitter)))
+            {
+                //! submitters can ONLY see their created tickets
+                model = currentUser.CreatedTickets
+                    .Select(ticket => TicketIndexViewModel.CreateViewModel(ticket))
+                    .ToList();
+            }
+            else if (User.IsInRole(nameof(UserRolesEnum.Developer)))
+            {
+                //! developers can ONLY see their assigned tickets
+                model = currentUser.AssignedTickets
+                    .Select(ticket => TicketIndexViewModel.CreateViewModel(ticket))
+                    .ToList();
+            }
+            else
+            {
+                //! if user isn't in any of the roles above redirect home
+                return RedirectToAction(nameof(HomeController.Index), new { controller = "Home" });
+            }
 
             return View(model);
         }
@@ -38,52 +73,62 @@ namespace BugTracker.Controllers
         // GET: Ticket/Details/{id}
         public ActionResult Details(Guid id)
         {
+            ViewBag.OverrideCurrentPage = "ticket-index";
+            if (Guid.Empty == id || string.IsNullOrWhiteSpace(id.ToString()))
+            {
+                return RedirectToAction(nameof(TicketController.Index));
+            }
+
+            TicketDetailsViewModel model = TicketDetailsViewModel.CreateNewViewModel(TicketRepository.GetTicket(id), DbContext);
+            
+            return View(model);
+        }
+
+        // GET: Ticket/Create
+        public ActionResult Create()
+        {
+            ViewBag.OverrideCurrentPage = "ticket-index";
             return View();
         }
 
-        //// GET: Ticket/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
+        // POST: Ticket/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
 
-        //// POST: Ticket/Create
-        //[HttpPost]
-        //public ActionResult Create(FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add insert logic here
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
+        // GET: Ticket/Edit/{id}
+        public ActionResult Edit(int id)
+        {
+            ViewBag.OverrideCurrentPage = "ticket-index";
+            return View();
+        }
 
-        //// GET: Ticket/Edit/{id}
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
+        // POST: Ticket/Edit/{id}
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
 
-        //// POST: Ticket/Edit/{id}
-        //[HttpPost]
-        //public ActionResult Edit(int id, FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add update logic here
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
         //// GET: Ticket/Delete/{id}
         //public ActionResult Delete(int id)
---
+++ BugTracker/Models/ViewModels/Ticket/TicketDetailsViewModel.cs
@@ -0,0 +1,61 @@
+﻿using BugTracker.Models.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace BugTracker.Models.ViewModels.Ticket
+{
+    public class TicketDetailsViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+        public string Type { get; set; }
+
+        public List<TicketAttachments> Attachments { get; set; }
+
+        public List<TicketComments> Comments { get; set; }
+
+        public Domain.Project Project { get; set; }
+        public Guid ProjectId { get; set; }
+
+        public HelperUserViewModel Author { get; set; }
+        public HelperUserViewModel AssignedUser { get; set; }
+
+        public static TicketDetailsViewModel CreateNewViewModel(Domain.Ticket ticket, ApplicationDbContext dbContext)
+        {
+            if (ticket == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            try
+            {
+                return new TicketDetailsViewModel()
+                {
+                    Id = ticket.Id,
+                    Title = ticket.Title,
+                    Description = ticket.Description,
+                    Priority = ticket.Priority.PriorityString,
+                    Status = ticket.Status.StatusString,
+                    Type = ticket.Type.TypeString,
+                    Comments = ticket?.Comments ?? new List<TicketComments>(),
+                    Attachments = ticket?.Attachments ?? new List<TicketAttachments>(),
+                    DateCreated = ticket.DateCreated,
+                    DateUpdated  = ticket.DateUpdated,
+                    Author = HelperUserViewModel.CreateNewViewModel(ticket.Author, dbContext),
+                    AssignedUser = HelperUserViewModel.CreateNewViewModel(ticket.AssignedUser, dbContext),
+                    Project = ticket.Project,
+                    ProjectId = ticket.ProjectId,
+                };
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
