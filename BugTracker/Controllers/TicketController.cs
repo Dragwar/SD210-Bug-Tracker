387f4c7422213c6f20a371116e6b0b5e3c6e2c51
Everett Grassler <dragwar9@gmail.com>
2019-04-14 23:05:26 -0500

    Added basic ticket create page

--- BugTracker/Controllers/TicketController.cs
+++ BugTracker/Controllers/TicketController.cs
@@ -1,4 +1,5 @@
 ﻿using BugTracker.Models;
+using BugTracker.Models.Domain;
 using BugTracker.Models.ViewModels.Ticket;
 using BugTracker.MyHelpers;
 using BugTracker.MyHelpers.DB_Repositories;
@@ -17,6 +18,7 @@ namespace BugTracker.Controllers
         private readonly TicketRepository TicketRepository;
         private readonly UserRoleRepository UserRoleRepository;
         private readonly UserRepository UserRepository;
+        private readonly ProjectRepository ProjectRepository;
 
         public TicketController()
         {
@@ -24,6 +26,7 @@ namespace BugTracker.Controllers
             TicketRepository = new TicketRepository(DbContext);
             UserRepository = new UserRepository(DbContext);
             UserRoleRepository = new UserRoleRepository(DbContext);
+            ProjectRepository = new ProjectRepository(DbContext);
         }
 
         // GET: Ticket
@@ -80,31 +83,165 @@ namespace BugTracker.Controllers
             }
 
             TicketDetailsViewModel model = TicketDetailsViewModel.CreateNewViewModel(TicketRepository.GetTicket(id), DbContext);
-            
+
             return View(model);
         }
 
         // GET: Ticket/Create
+        [Authorize(Roles = nameof(UserRolesEnum.Submitter))]
         public ActionResult Create()
         {
             ViewBag.OverrideCurrentPage = "ticket-index";
-            return View();
+            string userId = User.Identity.GetUserId();
+
+            if (UserRoleRepository.IsUserInRole(userId, UserRolesEnum.Submitter))
+            {
+                throw new Exception("This shouldn't happen");
+            }
+
+            List<SelectListItem> userProjects = ProjectRepository
+                .GetUserProjects(userId)
+                .Select(project => new SelectListItem()
+                {
+                    Text = project.Name,
+                    Value = project.Id.ToString(),
+                })
+                .ToList();
+
+            if (!userProjects?.Any() == null)
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+
+            TicketCreateViewModel model = new TicketCreateViewModel()
+            {
+                AuthorId = userId,
+                Projects = userProjects,
+            };
+
+            return View(model);
         }
 
         // POST: Ticket/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [Authorize(Roles = nameof(UserRolesEnum.Submitter))]
+        public ActionResult Create(TicketCreateViewModel formData)
         {
+            if (formData == null || !ModelState.IsValid || !formData.Type.HasValue || !formData.Priority.HasValue)
+            {
+                ModelState.AddModelError("", "Error - Bad data");
+                formData = GenerateCreateViewModel(formData.ProjectId, formData) ?? throw new Exception("bad data");
+
+                return View(formData);
+            }
+
+            // TODO: Move this check to the edit method for tickets and decide whether to remove this
+            //! because only admins/project managers can give tickets statuses
+            //! BUT only submitters can create tickets, therefore this check is useless
+            if (!formData.Status.HasValue)
+            {
+                if (User.IsInRole(nameof(UserRolesEnum.Submitter)) && !User.IsInRole(nameof(UserRolesEnum.Admin)) && !User.IsInRole(nameof(UserRolesEnum.ProjectManager)))
+                {
+                    formData.Status = TicketStatusesEnum.Open;
+                }
+                else
+                {
+                    ModelState.AddModelError(nameof(TicketCreateViewModel.Status), "You need to provide a ticket status");
+                    formData = GenerateCreateViewModel(formData.ProjectId, formData) ?? throw new Exception("bad data");
+
+                    return View(formData);
+                }
+            }
+
             try
             {
-                // TODO: Add insert logic here
+                Project foundProject = ProjectRepository.GetProject(formData.ProjectId) ?? throw new Exception();
+                ApplicationUser foundAuthor = UserRepository.GetUserById(formData.AuthorId) ?? throw new Exception();
 
-                return RedirectToAction("Index");
+
+                //// TODO: Maybe this is a bad way of doing this, maybe I should query the database?
+                //bool validTicketAttributes = false;
+                //validTicketAttributes = CONSTANTS.TicketTypes.TryGetValue(formData.Type.Value, out TicketTypes type);
+                //validTicketAttributes = CONSTANTS.TicketPriorites.TryGetValue(formData.Priority.Value, out TicketPriorities priority);
+                //validTicketAttributes = CONSTANTS.TicketStatuses.TryGetValue(formData.Status.Value, out TicketStatuses status);
+
+                //if (!validTicketAttributes)
+                //{
+                //    ModelState.AddModelError("", "Invalid data");
+                //    formData = GenerateCreateViewModel(formData.ProjectId, formData) ?? throw new Exception("bad data");
+                //    return View(formData);
+                //}
+
+                TicketPriorities priority = DbContext.TicketPriorities.First(tp => tp.Id == (int)formData.Priority.Value);
+                TicketTypes type = DbContext.TicketTypes.First(tt => tt.Id == (int)formData.Type.Value);
+                TicketStatuses status = DbContext.TicketStatuses.First(ts => ts.Id == (int)formData.Status.Value);
+
+
+                Ticket newTicket = new Ticket()
+                {
+                    Title = formData.Title,
+                    Description = formData.Description,
+                    PriorityId = status.Id,
+                    Priority = priority,
+                    StatusId = status.Id,
+                    Status = status,
+                    TypeId = type.Id,
+                    Type = type,
+                    Author = foundAuthor,
+                    AuthorId = foundAuthor.Id,
+                    Project = foundProject,
+                    ProjectId = foundProject.Id,
+                };
+
+                foundAuthor.CreatedTickets.Add(newTicket);
+                foundProject.Tickets.Add(newTicket);
+
+                DbContext.SaveChanges();
+
+
+                return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception e)
             {
-                return View();
+                ModelState.AddModelError("", "Error - Bad data");
+                ModelState.AddModelError("", e.Message); // TODO: Remove after project completion (on staging phase)
+                formData = GenerateCreateViewModel(formData.ProjectId, formData) ?? throw new Exception("bad data");
+
+                return View(formData);
             }
+        }
+
+        private TicketCreateViewModel GenerateCreateViewModel(Guid projectId, TicketCreateViewModel formData)
+        {
+            ViewBag.OverrideCurrentPage = "ticket-index";
+            string userId = User.Identity.GetUserId();
+            List<SelectListItem> userProjects = ProjectRepository
+                .GetUserProjects(userId)
+                .Select(project => new SelectListItem()
+                {
+                    Text = project.Name,
+                    Value = project.Id.ToString(),
+                })
+                .ToList();
+
+            if (!userProjects?.Any() == null)
+            {
+                return null;
+            }
+
+            TicketCreateViewModel model = new TicketCreateViewModel()
+            {
+                Title = formData?.Title,
+                Description = formData?.Description,
+                Priority = formData?.Priority,
+                Status = formData?.Status,
+                Type = formData?.Type,
+                Projects = userProjects,
+                ProjectId = formData.ProjectId == Guid.Empty ? projectId : formData.ProjectId, // can't be left empty or null
+                AuthorId = formData?.AuthorId,
+            };
+
+            return model;
         }
 
         // GET: Ticket/Edit/{id}
---
+++ BugTracker/Models/ViewModels/Ticket/TicketCreateViewModel.cs
@@ -0,0 +1,34 @@
+﻿using BugTracker.MyHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace BugTracker.Models.ViewModels.Ticket
+{
+    public class TicketCreateViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public TicketPrioritiesEnum? Priority { get; set; }
+
+
+        [Required]
+        public TicketTypesEnum? Type { get; set; }
+
+        public TicketStatusesEnum? Status { get; set; }
+
+        public List<SelectListItem> Projects { get; set; }
+
+        [Required]
+        [Display(Name = "Project")]
+        public Guid ProjectId { get; set; }
+
+        public string AuthorId { get; set; }
+    }
+}
